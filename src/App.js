da7c08e8cd499e8ab7199337a635ef8f30bc228d
mehedisarkar2k <mehedisarkar@hotmail.com>
2021-11-30 13:32:46 +0600

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import { AiFillGithub } from "react-icons/ai";
-import { BsFacebook, BsLinkedin } from "react-icons/bs";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl m-5 text-purple-800 font-bold tracking-wider">
-        Mehedi Hasan Sarkar
-      </h1>
-      <div className="flex items-center justify-center text-3xl space-x-4 text-purple-700">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="transition hover:text-purple-400 transform hover:scale-110 hover:shadow-lg"
-          href="https://github.com/mehedisarkar2k"
-        >
-          <AiFillGithub />
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="transition hover:text-purple-400 transform hover:scale-110 hover:shadow-lg"
-          href="https://www.linkedin.com/in/mehedisarkar2k/"
-        >
-          <BsLinkedin />
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="transition hover:text-purple-400 transform hover:scale-110 hover:shadow-lg"
-          href="https://www.facebook.com/MehediSarkar2k"
-        >
-          <BsFacebook />
-        </a>
-      </div>
+      <h1 className="text-primary-light">Hello Mehedi Hasan Sarkar</h1>
+      <h2 className="text-gray">I'm full stack web developer</h2>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,29 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: {
+        light: "#009ada",
+        DEFAULT: "#04c2c9",
+        dark: "#00a1a7",
+      },
+      secondary: {
+        light: "",
+        DEFAULT: "#e31b6d",
+        dark: "",
+      },
+      white: "#616161",
+      dark: "#444649",
+      gray: "#616161",
+      "dark-blue": "#252934",
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active", "focus", "group-hover"],
+      textDecoration: ["focus-visible"],
+    },
   },
   plugins: [],
 };
