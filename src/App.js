e6809c35b89ae7b004f0908987d3f4c09e21f2b2
mehedisarkar2k <mehedisarkar@hotmail.com>
2021-11-30 17:54:52 +0600

    added reacr router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Banner from "./components/Banner/Banner";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="flex flex-col items-center min-h-screen w-full">
-      <Banner />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Banner />
+            <Nav />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import Classes from "./Nav.module.css";
+
+const Nav = () => {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > "100");
+    });
+  }, []);
+  console.log(scroll);
+
+  return (
+    <div className="bg-dark-blue w-full shadow-sm">
+      <div className={`my-container ${Classes.Nav}`}>
+        <nav className="py-4">
+          <ul className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8 text-xl font-light tracking-wide">
+            <li>
+              <NavLink className="text-white" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="text-white" to="/">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="text-white" to="/">
+                Portfolio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="text-white" to="/">
+                Blog
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="text-white" to="/">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
