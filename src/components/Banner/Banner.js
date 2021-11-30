09e063d69e855ad42b5c46b0580a7730e379e711
mehedisarkar2k <mehedisarkar@hotmail.com>
2021-11-30 13:49:50 +0600

    added some content to banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>This is Banner Section</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="my-container text-center">
+        <h1>This is Banner Section</h1>
+        <p className="text-justify">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          labore molestiae nemo nisi doloremque amet! Nam quos molestias, quidem
+          debitis facere ullam, expedita quo cumque dicta corrupti totam
+          dignissimos quia?
+        </p>
+      </div>
     </div>
   );
 };
