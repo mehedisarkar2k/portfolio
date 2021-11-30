2b047c88396d2809a551a731eb7fc8c98745def2
mehedisarkar2k <mehedisarkar@hotmail.com>
2021-11-30 13:42:07 +0600

    banner part added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-primary-light">Hello Mehedi Hasan Sarkar</h1>
-      <h2 className="text-gray">I'm full stack web developer</h2>
+    <div className="flex flex-col items-center min-h-screen">
+      <Banner />
     </div>
   );
 }
