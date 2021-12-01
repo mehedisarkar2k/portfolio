45f030d6e0e7d1babd58cf192488b4cfc74ad045
MdWahiduzzamanEmon <wahedemon09@gmail.com>
2021-12-01 13:24:29 +0600

    email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,42 @@
 import React from "react";
 import Classes from "./Contact.module.css";
+import { init } from "emailjs-com";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
+init("user_4iTNkPRIOlI18bEZPuMdM");
 
 const Contact = () => {
+  // const [fieldItems, setFieldItems] = React.useState({});
+  const [name, setName]=React.useState('')
+  const [email, setEmail]=React.useState('')
+  const [message, setMessage]=React.useState('')
+ 
+  const handleContact = (e) => {
+    e.preventDefault();
+    const fieldItems = {
+      name: name,
+      email: email,
+      message: message
+    }
+  //  console.log(fieldItems);
+  emailjs
+    .send(
+      "service_r2vkaos", //service id
+      "template_9a9b0xe", //template id
+      fieldItems,// field object
+      "user_4iTNkPRIOlI18bEZPuMdM" //user id
+    )
+    .then(
+      function (response) {
+        Swal.fire("Good job!", "Thanks for your message!", "success");
+      },
+      function (error) {
+        Swal.fire("Sorry!", "Something went wrong!Try again", "error");
+      }
+    );
+    e.target.reset();
+}
+
   return (
     <div className={`${Classes.Contact} bg-dark-blue w-full mt-20 pt-20`}>
       <div className="my-container">
@@ -13,7 +48,7 @@ const Contact = () => {
           <p className="text-primary-light text-center mt-2 text-xl tracking-wide">
             Have a question or want to work together?
           </p>
-          <form>
+          <form onSubmit={handleContact}>
             <div className="relative mb-4 ">
               <label
                 htmlFor="name"
@@ -26,6 +61,7 @@ const Contact = () => {
                 id="name"
                 name="name"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className="relative mb-4 ">
@@ -37,9 +73,10 @@ const Contact = () => {
               </label>
               <input
                 type="email"
-                id="name"
-                name="name"
+                id="email"
+                name="email"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
@@ -54,11 +91,15 @@ const Contact = () => {
                 id="message"
                 name="message"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                onChange={(e) => setMessage(e.target.value)}
               />
             </div>
 
             <div className="flex items-center justify-center">
-              <button className="btn bg-primary-light px-8 text-xl text-white tracking-wider hover:bg-primary-dark rounded-lg">
+              <button
+                type="submit"
+                className="btn bg-primary-light px-8 text-xl text-white tracking-wider hover:bg-primary-dark rounded-lg"
+              >
                 Send Me
               </button>
             </div>
