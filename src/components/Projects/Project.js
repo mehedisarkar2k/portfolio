7b47f5a772f05bfb2e1d8977ed7df8e05d765211
mehedisarkar2k <mehedisarkar@hotmail.com>
2021-12-01 12:04:23 +0600

    update to others

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import Underline from '../Underline/Underline';
-import SingleProjects from './SingleProjects';
-
-
+import React, { useEffect, useState } from "react";
+import Underline from "../Underline/Underline";
+import SingleProjects from "./SingleProjects";
 
 const Project = () => {
-    const [projects , setProjects] = useState([])
-    useEffect(() => {
-        fetch('./data.json')
-        .then(res => res.json())
-        .then(data => setProjects(data))
-    }, [])
-    return (
-        <div className="my-container">
-            <h1 className="text-center">PROJECTS</h1>
-            <Underline/>
-            <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3">
-                {
-                    projects.map(project => <SingleProjects project={project} key={project.id}/>)
-                }
-            </div>
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setProjects(data));
+  }, []);
+  return (
+    <div style={{ background: "#D3D3D3", width: "100%" }}>
+      <div className="my-container">
+        <h1 className="text-center">PROJECTS</h1>
+        <Underline />
+        <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3">
+          {projects.map((project) => (
+            <SingleProjects project={project} key={project.id} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Project;
